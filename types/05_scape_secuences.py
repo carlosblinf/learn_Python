f9eff47baecba010f226ec4b703b6d31a59fb907
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-01-31 14:43:49 -0500

    scape secuences

---
+++ types/05_scape_secuences.py
@@ -0,0 +1,8 @@
+""" Secuencias de escape """
+
+curso_java = 'Curso de "Java"'
+curso_python = "Curso de \"Python\""
+curso = "Curso de \nProgramacion"
+print(curso_java)
+print(curso_python)
+print(curso)
