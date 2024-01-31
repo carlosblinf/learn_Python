9d1214da56fd9d88bc62c32f0bda5d33e1f05579
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-01-31 16:33:16 -0500

    while loop

---
+++ flow_control/while.py
@@ -0,0 +1,14 @@
+""" Flujo while """
+
+n = 1
+while True:
+    print(n)
+    n += 1
+    if n > 5:
+        print("termine")
+        break
+
+numero = 1
+while numero < 100:
+    print(numero)
+    numero *= 2
