6e7fc5ee8e2f7b06bf5a62db9747139dfbd7a955
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-01-31 15:02:55 -0500

    number functions

---
+++ types/07_number_functions.py
@@ -0,0 +1,15 @@
+""" Funciones de numeros """
+import math
+
+# native
+print(round(1.3))
+print(round(1.7))
+print(abs(-5))
+
+# using math
+print(math.ceil(1.1))  # print 2
+print(math.floor(1.9999))  # print 1
+print(math.isnan(15))  # False
+# print(math.isnan("15"))  # Error
+print(math.pow(10, 3))  # 1000
+# sqrt, etc
