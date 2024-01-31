04be99bd13d53cea0aacd13bba85014f1ce2008e
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-01-31 14:00:05 -0500

    format strings

---
+++ types/03_format_strings.py
@@ -0,0 +1,9 @@
+"""format strings"""
+nombre = "Carlos"
+apellido = "Brooks"
+
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+nombre_completo_format = f"mi nombre es: {nombre} {apellido}"
+print(nombre_completo_format)
