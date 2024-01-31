76103ebd8d32be758f0fe7969ee192a515ed2e46
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-01-31 15:37:04 -0500

    if flow

---
+++ flow_control/02_if.py
@@ -0,0 +1,13 @@
+""" flujo if """
+
+edad = int(input("inserte la edad:\n"))
+
+if edad > 54:
+    print("Puede ver la pelicula con descuento")
+elif edad > 17:
+    print("Puede ver la pelicula")
+else:
+    print("No puede ver a pelicula")
+    print("ve a otro lado")
+
+print("Listo")
