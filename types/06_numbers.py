bc18bea473c0a606090cb268d034e6a84e3b5d75
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-01-31 14:52:57 -0500

    numbers

---
+++ types/06_numbers.py
@@ -0,0 +1,19 @@
+""" Numeros """
+
+numero = 2  # Integer
+decimal = 1.2  # Float
+imaginario = 2 + 2j  # Imaginario 2 + 2i
+
+print(1 + 2)
+print(1 - 2)
+print(2 * 2)
+print(1 / 3)
+print(1 // 3)  # no decimal
+print(8 % 3)
+print(2 ** 3)  # pow
+
+numero += 2
+numero -= 3
+numero *= 4
+numero /= 5
+print("numero", numero)
