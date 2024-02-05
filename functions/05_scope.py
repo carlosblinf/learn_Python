d69c4be54809d4fc212769e14673096ac72ebbd2
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-02-05 13:09:32 -0500

    scope 2

--- functions/05_scope.py
+++ functions/05_scope.py
@@ -1,12 +1,20 @@
 """ Scope o alcance """
 
+# global scope
+saludo = "Hola a todos"
+
 
 def saludar():
+    # inner scope saludar
     saludo = "Hola mundo"
+    print(saludo)
 
 
 def saludarPersona():
+    # inner scope saludarPersona
     saludo = "Hola Carlos"
+    print(saludo)
 
 
-# print(saludo) saludo is not defined
+saludar()
+saludarPersona()
