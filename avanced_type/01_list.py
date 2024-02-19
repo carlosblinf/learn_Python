60bf3f0611ebc0c4959e0b56ad35b7c9c49d76ea
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-02-19 18:09:45 -0500

    lists

---
+++ avanced_type/01_list.py
@@ -0,0 +1,17 @@
+""" Listas """
+
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["hola", "mundo", "Carlos", "disfruta"]
+booleans = [True, False, True, True, False]
+matriz = [[0, 1], [1, 0]]
+ceros = [0] * 10
+muchos = [0, 1] * 5
+print(muchos)
+
+alfanumericos = numeros + letras
+print(alfanumericos)
+rango = list(range(10))
+print(rango)
+chars = list("hola mundo")
+print(chars)
