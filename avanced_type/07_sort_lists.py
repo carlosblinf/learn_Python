0c16ffae71cdd2ba605a609e3d869258f3330e6d
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-02-19 19:07:51 -0500

    sort list

---
+++ avanced_type/07_sort_lists.py
@@ -0,0 +1,29 @@
+""" ordenando listas """
+
+numeros = [5, 2, 7, 29, 1, 9]
+
+numeros.sort()
+print(numeros)
+numeros.sort(reverse=True)
+print(numeros)
+# devuelve una nueva lista sin afectar la anterior
+nuevos = sorted(numeros, reverse=False)
+print(nuevos)
+
+# ordena por el primer elemento
+usuarios = [[4, "Carlos"], [2, "Felipe"], [5, "Pulga"]]
+usuarios.sort()
+print(usuarios)
+usuarios = [["Pulga", 4], ["Felipe", 2], ["Carlos", 5]]
+usuarios.sort()
+print(usuarios)
+
+
+def ordena(elemento):
+    return elemento[1]
+
+
+usuarios.sort(key=ordena)
+print(usuarios)
+usuarios.sort(key=ordena, reverse=True)
+print(usuarios)
