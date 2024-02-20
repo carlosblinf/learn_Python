99693f1542041f23a95de83ea78880c5f3a683a0
Carlos Ernesto Brooks Lorie <carlosblinf@gmail.com>
2024-02-19 19:30:06 -0500

    map and filter

--- avanced_type/08_comprension_list.py
+++ avanced_type/08_comprension_list.py
@@ -16,3 +16,8 @@ nombres = [usuario[0] for usuario in usuarios]  # transformando lista
 print(nombres)
 nombres_filtrados = [usuario[0] for usuario in usuarios if usuario[1] > 2]
 print(nombres_filtrados)
+
+nombres1 = list(map(lambda usuario: usuario[0], usuarios))
+print(nombres1)
+menos_usuarios = list(filter(lambda usuario: usuario[1] > 2, usuarios))
+print(menos_usuarios)
